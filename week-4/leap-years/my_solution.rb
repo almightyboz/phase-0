9eab4ae15533f01f6d7ffe6d32166bb50cdbfa84
Emily <ebosakowski@gmail.com>
2015-12-30 17:09:33 -0600

    Fixing formatting

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -1,6 +1,8 @@
 # Leap Years
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with: Kathryn
+
+# Your Solution Below
 
 def leap_year?(year)
   if year % 4 == 0 && year % 100 != 0
@@ -14,19 +16,18 @@ def leap_year?(year)
   end
 end
 
-def leap_year?(year)
-  if year % 4 == 0
-    if year % 100 != 0
-      return true
-    elsif year % 100 == 0 && year % 400 != 0
-      return false
-    elsif year % 400 == 0
-      return true
-    end
-  else
-    return false
-  end
-end
+# def leap_year?(year)
+#   if year % 4 == 0
+#     if year % 100 != 0
+#       return true
+#     elsif year % 100 == 0 && year % 400 != 0
+#       return false
+#     elsif year % 400 == 0
+#       return true
+#     end
+#   else
+#     return false
+#   end
+# end
 
 
-# Your Solution Below
