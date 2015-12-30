4c783cee20b6c9c5cace166cae24320707cac039
Emily <ebosakowski@gmail.com>
2015-12-30 16:47:43 -0600

    Initial commit

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,14 @@
+# Good Guess
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def good_guess?(value)
+  if value == 42
+    return true
+  else
+    return false
+  end
+end
