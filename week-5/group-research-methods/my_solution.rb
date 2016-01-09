3a69d1c8b5a6018479a2943fcceacb9634bce20b
Emily <ebosakowski@gmail.com>
2016-01-09 13:15:48 -0600

    Adding Ruby Docs tips

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -1,6 +1,6 @@
 # Research Methods
 
-# I spent [] hours on this challenge.
+# I spent 1 hours on this challenge.
 
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
@@ -49,6 +49,8 @@ end
 # My array finding method first creates an empty list that I will populate with the correct answers. Then I perform a check on each list item in turn. When I perform the check, I'm testing if the "the thing to find" string is contained within that particular list item. If it is, I take that list item (converted to a string) and add it to my answer list. If that particular item doesn't have the search string, the check is completed and the computer moves to the next word on the source list. Once I've tested every individual object on my source list, I output the list of words that contain the thing to find.
 #
 #My hash finding method performs a check by iterating through each key-value pairing in the source hash. Specifically, the check is whether the thing we want to find is identical to a pairing's value. (I know this because the test string, pet names are keys and their ages are values.) If a pairing's value is identical to the thing to find, I add the pairing's key to a list. After I've iterated through every pairing, I return the list of correct answers.
+#
+# I use ctrl+f excessively to search through the ruby docs for key words. That way I can mentally connect several methods that operate along similar lines. And when I have trouble understanding a concept, I usually just copy the words into google and add "ruby", and read the first three results I get. Especially if it's StackOverflow posts. Because the people there 1) use specific examples of how a piece of code is used on a problem and 2) suggest different ways of utilizing different methods and approaches on the same problem. That helps me understand how a methods *is* used, and the Ruby Docs provide the intellectual context for how that same method *should be* used.
 
 # Person 2
 def my_array_modification_method!(source, thing_to_modify)
