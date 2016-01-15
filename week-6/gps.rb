eb70cc18a0fe7b7b6d294fe07a299699f26a9c42
Emily <ebosakowski@gmail.com>
2016-01-14 22:30:48 -0600

    Refactoring with J. Silvestri

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,42 +1,33 @@
 # Your Names
-# 1)
-# 2)
+# 1) Emily(me)
+# 2) Jonathan S.
 
-# We spent [#] hours on this challenge.
+# We spent [2] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, num_of_ingredients)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+  cookbook = {"cookie" => 1, "cake" =>  5, "pie" => 7}
+  cookbook.include?(item_to_make)
+  raise ArgumentError.new("#{item_to_make} is not a valid input") if cookbook.include?(item_to_make) == false
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
-    end
-  end
+  serving_size = cookbook[item_to_make]
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+  no_leftovers = "Great! You can make #{num_of_ingredients / serving_size} #{item_to_make}!"
+  has_leftovers = no_leftovers + " You could make #{num_of_ingredients % serving_size} cookies with your leftovers."
 
-  serving_size = library.values_at(item_to_make)[0]
-  remaining_ingredients = num_of_ingredients % serving_size
-
-  case remaining_ingredients
-  when 0
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
-  else
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
-  end
+  return no_leftovers if num_of_ingredients % serving_size == 0
+  return has_leftovers if num_of_ingredients % serving_size != 0
 end
 
-p serving_size_calc("pie", 7)
+ p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
 p serving_size_calc("cake", 5)
-p serving_size_calc("cake", 7)
+ p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
+
+
