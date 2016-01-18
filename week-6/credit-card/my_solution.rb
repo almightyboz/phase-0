db4e83f049c6b2fdfcff47572258e9670ac23734
Emily <ebosakowski@gmail.com>
2016-01-18 00:17:22 -0600

    Adding refactors

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -2,8 +2,8 @@
 
 
 # I worked on this challenge with: Brian Bier.
-# I spent [#] hours on this challenge.
-
+# I spent 2 hours on this challenge.
+##############################################################
 # Pseudocode
 
 # Input: A number (should be 16 digits)
@@ -37,78 +37,89 @@
 #   IF no
 #       return false
 
+##############################################################
 # Initial Solution
 
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
 
 class CreditCard
-
+​
   def initialize(card_number)
     split_card = card_number.to_s.split("")
     if split_card.length != 16
-      raise ArgumentError.new("Not a valid creditcard")
+      raise ArgumentError.new("Not a valid credit card")
     end
-    @card_number = split_card
+     @card_number = split_card.map(&:to_i)
   end
-
-
-  # def check_card
-  #   index_counter = 0
-  #   @card_number.each do |num| #maybe change to map! later?
-  #     num = num.to_i
-  #     if @card_number[counter]
-  #       num *= 2
-  #       counter += 2
-  #     end
-  #   return @card_number
-  #   end
-  # end
-
-  def multiply_numbers
-    bigarray = []
-    #index_counter = 0
-    @card_number.map! do |num| #maybe change to map! later?
-      num = num.to_i
-    end
-    @card_number.each_slice(2) {|sublist| bigarray << sublist}
-    p bigarray
-    bigarray = bigarray.each{|sublist| sublist[0] *= 2}
-    p bigarray
-    @card_number = bigarray.flatten(1)
-  end
-
+​
   def check_card
+   new_array = []
+    for index in (0...@card_number.length)
+      if index.even?
+      new_array.push(@card_number[index] *= 2)
+    # puts "At position #{index}: #{reverse[index]}"
+      else
+      new_array.push(@card_number[index])
+      end
+    end
+     individual_card =new_array.join("").split('').map(&:to_i)
+​
+      sum = 0
+      individual_card.each do |i|
+        sum += i
+      end
+​
+    if  sum % 10  == 0
+       true
+    else
+      false
+    end
+​
   end
-
+​
 end
+​
+new_card = CreditCard.new(4408041234567901)
+new_card.check_card
 
-# end
-
-
-#p check_card(["0", "1", "2", "3", "4", "5", "6", "7"])
-
-
+##############################################################
 # Refactored Solution
 
-# ary = [0,1,2,3,4,5,6]
-# counter = 0
-# ary.each{|num| puts num}
+#REFACTOR
+​
+class CreditCard
+​
+  def initialize(card_number)
+    @card_number = card_number.to_s.split("").map(&:to_i)
+    if @card_number.length != 16
+      raise ArgumentError.new("Incorrect digit count for #{card_number}")
+    end
+  end
+​
+# http://ruby-doc.org/core-2.2.0/Enumerable.html#method-i-each_with_index
+  def check_card
+    formatted_cc_numbers = []
+    @card_number.each_with_index do |item,index|
+      if index.even?
+        formatted_cc_numbers.push(item * 2)
+      else
+        formatted_cc_numbers.push(item)
+      end
+    end
+
+    individual_card = formatted_cc_numbers.join("").split('').map(&:to_i)
+​
+   # sum = individual_card.inject{|sum,num| sum + num}
+    sum = individual_card.inject(&:+)
+    sum % 10  == 0
+  end
+end
+​
+new_card = CreditCard.new(4408041234567901)
+p new_card.check_card
 
 
-# bigarray = []
-# ary.each_slice(2){|n| bigarray << n }
-
-# puts bigarry.each{|sublist| sublist[0] *= 2}
-# p ary
-
-# counter = -2
-# while counter > -6
-
-
-
-
-
-
-
+##############################################################
 # Reflection
+# The most difficult part of this challenge was finding a way to modify every other item in the array. We tried .each_slice to middling success, but my partner was the one who decided that we use each_with_index and even? to do both. I had no idea that either could go either, or that control flow could work in that manner, but it made sense as soon as I understood it. And I'm glad I know those methods now, as well as the use of inject() when summing up an array of integers. (Using inject isn't difficult, it means one doesn't need a counter and an interating loop to sum an array, understanding inject() is difficult.)
